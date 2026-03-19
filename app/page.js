28e9dc6b5f794db52d324bc00e637233ce5a307d
Abdulrahman Mohammed <gabdulrahman.mohammed@gmail.com>
2026-03-19 17:25:53 +0200

    add arrow

--- app/page.js
+++ app/page.js
@@ -4,7 +4,17 @@ import Projects from "@/_ui/projects/Projects";
 export default function Home() {
   return (
     <div className="overflow-hidden">
-      <div className="h-screen bg-gray-900"></div>
+      <div className="h-screen bg-gray-900 flex items-center justify-center">
+        <svg
+          className="w-[100px] h-[100px] text-gray-500 animate-bounce "
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+        </svg>
+      </div>
       <News />
       <Projects />
     </div>
